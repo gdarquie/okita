b78d7cb69c0afd7f082edadbde5fd78b4aa9979f
gdarquie <gaetan.darquie@gmail.com>
2019-02-07 22:19:04 +0100

    upgrade synthesis dashboard

--- src/Controller/SynthesisController.php
+++ src/Controller/SynthesisController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Character;
+use App\Entity\Setting;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,6 +27,7 @@ class SynthesisController extends AbstractController
         return $this->render('synthesis.html.twig', [
             'character' => $this->getRandomCharacter(rand($stats['min_id'],$stats['max_id'])),
             'statsCharacter' => $stats,
+            'ratioSex' => $this->getRatioSex()
         ]);
     }
 
@@ -53,6 +55,39 @@ class SynthesisController extends AbstractController
     {
         $query = $this->em->createQuery('SELECT COUNT(c.id) as total,MAX((c.deathDate - c.birthDate)/(365*24*3600)) as max_age, MAX(c.id) as max_id, MIN(c.id) as min_id FROM '.Character::class.' c ');
         $stats = $query->getSingleResult();
+        
+        return $stats;
+    }
+
+    /**
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function getRatioSex()
+    {
+        $query = $this->em->createQuery('SELECT COUNT(c.id) as total FROM '.Character::class.' c WHERE c.sex = :sex GROUP BY c.sex');
+        $query->setParameter('sex', 'F');
+        $stats['female'] = $query->getSingleScalarResult();
+
+        $query = $this->em->createQuery('SELECT COUNT(c.id) as total FROM '.Character::class.' c WHERE c.sex = :sex GROUP BY c.sex');
+        $query->setParameter('sex', 'M');
+        $stats['male'] = $query->getSingleScalarResult();
+
+        return $stats;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatsFiction()
+    {
+        $query = $this->em->createQuery('SELECT s FROM '.Setting::class.' s WHERE s.key = :key');
+        $query->setParameter('key', '');
+        $stats['fiction_begin'] = $query->getResult();
+
+        $query = $this->em->createQuery('SELECT s FROM '.Setting::class.' s WHERE s.key = :key');
+        $query->setParameter('key', '');
+        $stats['fiction_end'] = $query->getResult();
 
         return $stats;
     }
