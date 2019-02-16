258f39d053bda98253287177b8e605b6cb50226b
gdarquie <gaetan.darquie@gmail.com>
2019-02-16 19:00:07 +0100

    minor correction for repository

--- src/Repository/ActionRepository.php
+++ src/Repository/ActionRepository.php
@@ -3,10 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\Action;
-use App\Entity\Character;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\ORM\Query\ResultSetMappingBuilder;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
