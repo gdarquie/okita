242a67688061b908561ac05a155038f48d8b23c0
gdarquie <gaetan.darquie@gmail.com>
2019-03-02 21:48:34 +0100

    Add command files

---
+++ src/Command/BuildRoutinesCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class BuildRoutinesCommand extends AbstractSQLCommand
+{
+    protected static $defaultName = 'app:build-routines';
+
+    protected function configure()
+    {
+
+        // ajouter les paramètres pour pouvoir créer les routines + ensuite créer un scénario qui génère des routines directement
+        // scenario1 : création de 10 routines et génération de personnages avec ces routines
+        $this
+            ->setDescription('Command for creating routines')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Routine name')
+            ->addArgument('content', InputArgument::OPTIONAL, 'Content of the routine')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $output->writeln([
+            'Routines generator',
+            '============',
+            '',
+        ]);
+
+        $name = $input->getArgument('name');
+        $content = $input->getArgument('content');
+
+        $statement = $this->connection->executeQuery('SELECT build_routine(\''.$name.'\', \''.$content.'\')');
+        $statement->fetchAll();
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+    }
+}
---
+++ src/Command/GenerateProjectCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\SQLService;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Yaml\Yaml;
+
+class GenerateProjectCommand extends AbstractSQLCommand
+{
+    protected static $defaultName = 'app:generate:project';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $output->writeln([
+            'Project generation begins',
+            '============',
+            '',
+        ]);
+
+        // Set project path
+        $rootPath = $this->container->get('kernel')->getProjectDir();
+        $projectPath = $rootPath.'/src/Domain/Project';
+        $projectFile = $projectPath.'/la-degradation.yaml';
+
+        // Parse project config
+        $value = Yaml::parseFile($projectFile);
+        $totalCharacters = $value['characters']['total'];
+        $routines = $value['routines'];
+        $io->success('Project pathes and config setted!');
+
+
+        // Launch commands
+
+        // Initialization
+        $output->writeln([
+            'Initialization is launched',
+            '============',
+            '',
+        ]);
+        (new SQLService($this->em))->initialize();
+        $io->success('Initialization succeeds!');
+
+
+        // Create Routines
+        $buildRoutinesCommand = $this->getApplication()->find('app:build-routines');
+
+        foreach ($routines as $routine) {
+            $key = (array_keys($routine))[0];
+            $value = $routine[$key];
+
+            $arguments = [
+                'command' => 'app:build-routines',
+                'name'    => $key,
+                'content'  => $value,
+            ];
+
+            $buildRoutinesCommandInput = new ArrayInput($arguments);
+            $buildRoutinesCommand->run($buildRoutinesCommandInput, $output);
+        }
+
+        //generate characters
+        $generateCharactersCommand = $this->getApplication()->find('app:generate:characters');
+
+        //generate actions? Another command?
+    }
+}
