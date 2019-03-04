58e3a9fb3cb0cc390d0db334cf567cca50af9911
gdarquie <gaetan.darquie@gmail.com>
2019-03-04 22:42:36 +0100

    Add action function

--- src/Component/SQL/ActionSQL.php
+++ src/Component/SQL/ActionSQL.php
@@ -35,23 +35,90 @@ class ActionSQL
     /**
      * @return string
      */
-    public function getGenerateAction()
+    public function getGenerateActionForDay()
     {
         $sql = <<< EOT
-        CREATE OR REPLACE FUNCTION generate_actions(start bigint, finish bigint)
+        CREATE OR REPLACE FUNCTION generate_actions_for_day(day INT)
           RETURNS VOID AS $$
         DECLARE
-          duration BIGINT;
+          temprow RECORD;
         BEGIN
-          duration := ROUND((finish-start) / 10);
-          LOOP
-        --     RAISE NOTICE 'Value: %', count;
-            PERFORM generate_turn_actions(start);
-            start := start+10;
+           FOR temprow IN
+                SELECT id, birth_date, death_date FROM character
+            LOOP
+                PERFORM generate_actions_for_day_and_character(day, temprow.id);
+            END LOOP;
         
-            IF start > finish THEN
-                EXIT;  -- exit loop
-            END IF;
+        END
+        $$
+        LANGUAGE plpgsql;
+        EOT;
+
+        return $sql;
+    }
+
+    /**
+     * @return string
+     */
+    public function generate_actions_for_day_and_character()
+    {
+        $sql = <<< EOT
+        CREATE OR REPLACE FUNCTION generate_actions_for_day_and_character(day INT, v_character_id INT) RETURNS VOID AS $$
+
+        DECLARE
+          start_day BIGINT;
+          end_day BIGINT;
+          v_routine RECORD;
+          v_habit RECORD;
+          v_count INTEGER;
+          v_nb_habits INTEGER;
+        
+        BEGIN
+          start_day := (day*(3600*24));
+        
+          -- commencer par récupérer la dernière action?
+          -- ???
+        
+          -- get routine from character
+          v_routine = (SELECT r
+            FROM routine r
+            INNER JOIN character_routine cr on r.id = cr.routine_id
+            WHERE cr.character_id = v_character_id LIMIT 1);
+        
+          v_nb_habits := (SELECT COUNT(*) FROM habit WHERE routine_id = v_routine.id);
+          v_count := 1;
+        
+          -- get all habits
+          -- for each habit, insert action
+           FOR v_habit IN
+              SELECT name, start, "end" as v_end FROM habit WHERE routine_id = v_routine.id
+            LOOP
+            PERFORM insert_action(v_habit.name, (start_day + v_habit.start), (start_day + v_habit.v_end), v_character_id);
+           v_count := v_count + 1;
+            END LOOP;
+        
+        END;
+        $$ LANGUAGE plpgsql;
+        EOT;
+
+        return $sql;
+    }
+
+    /**
+     * @return string
+     */
+    public function generateActionFromDayToDay()
+    {
+        $sql = <<< EOT
+        CREATE OR REPLACE FUNCTION generateActionFromDayToDay(v_start integer, v_end integer)
+        RETURNS VOID AS $$
+        DECLARE
+          v_count integer;
+        BEGIN
+          v_count := v_start;
+          WHILE v_count <= v_end LOOP
+            PERFORM generate_actions_for_day(v_count);
+            v_count := v_count +1;
           END LOOP;
         END
         $$
@@ -59,5 +126,6 @@ class ActionSQL
         EOT;
 
         return $sql;
+
     }
 }
