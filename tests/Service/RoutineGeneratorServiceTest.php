4d60ff8f145c710c8ce859eb23c5a340d623c67a
gdarquie <gaetan.darquie@gmail.com>
2019-03-26 23:31:40 +0100

    Comment routine test

--- tests/Service/RoutineGeneratorServiceTest.php
+++ tests/Service/RoutineGeneratorServiceTest.php
@@ -7,18 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RoutineGeneratorServiceTest extends WebTestCase
 {
-    /**
-     * @var RoutineGeneratorService
-     */
-    private $routineGenerator;
-
-    public function setUp()
-    {
-        parent::__construct();
-        $kernel = static::bootKernel([]);
-        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-        $this->routineGenerator = new RoutineGeneratorService($em);
-    }
+//    /**
+//     * @var RoutineGeneratorService
+//     */
+//    private $routineGenerator;
+//
+//    public function setUp()
+//    {
+//        parent::__construct();
+//        $kernel = static::bootKernel([]);
+//        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+//        $this->routineGenerator = new RoutineGeneratorService($em);
+//    }
 
 //    public function testGetActions()
 //    {
