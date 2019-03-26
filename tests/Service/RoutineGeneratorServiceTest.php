014c96e54ac86ec14f2c9b90983585efde7e7a99
gdarquie <gaetan.darquie@gmail.com>
2019-03-26 23:43:25 +0100

    Restore test for routine generator service

--- tests/Service/RoutineGeneratorServiceTest.php
+++ tests/Service/RoutineGeneratorServiceTest.php
@@ -7,25 +7,26 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RoutineGeneratorServiceTest extends WebTestCase
 {
-//    /**
-//     * @var RoutineGeneratorService
-//     */
-//    private $routineGenerator;
-//
-//    public function setUp()
-//    {
-//        parent::__construct();
-//        $kernel = static::bootKernel([]);
-//        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-//        $this->routineGenerator = new RoutineGeneratorService($em);
-//    }
+    /**
+     * @var RoutineGeneratorService
+     */
+    private $routineGenerator;
 
-//    public function testGetActions()
-//    {
-//        $routines = $this->routineGenerator->getActions([ 'travailler', 'jouer', 'danser']);
-//        $this->assertEquals([ 'dormir', 'travailler', 'jouer', 'danser','dormir'], $routines);
-//
-//    }
+    public function setUp()
+    {
+        parent::__construct();
+        $kernel = static::bootKernel([]);
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $this->routineGenerator = new RoutineGeneratorService($em);
+    }
+
+    public function testGetActions()
+    {
+        $routines = $this->routineGenerator->getActions([ 'travailler', 'jouer', 'danser']);
+        $this->assertEquals('dormir', $routines[0]);
+
+
+    }
 
 //    public function testCreateRoutines()
 //    {
